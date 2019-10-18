0f94efdc5516182c128cbfa07f8c698832de6942
rymcmahon <toedragdangle9@gmail.com>
2019-10-17 21:56:59 -0400

    added pickup_days index table and edit action

--- app/controllers/pickup_days_controller.rb
+++ app/controllers/pickup_days_controller.rb
@@ -1,24 +1,37 @@
 class PickupDaysController < ApplicationController
-    def index
+  def index
+    @pickup_days = current_organization.pickup_days.all
+  end
 
+  def new
+    @pickup_day = current_organization.pickup_days.new
+  end
+
+  def create
+    @pickup_day = current_organization.pickup_days.new(pickup_day_params)
+
+    if @pickup_day.save
+      redirect_to pickup_days_path
+    else
+      render 'new'
     end
+  end
 
-    def new
-      @pickup_day = current_organization.pickup_days.new
-    end
+  def edit
+    @pickup_day = current_organization.pickup_days.find(params[:id])
+  end
 
-    def create
-      @pickup_day = current_organization.pickup_days.new(pickup_day_params)
+  def update
 
-      if @pickup_day.save
-        redirect_to pickup_days_path
-      else
-        render 'new'
-      end
-    end
+  end
 
-    private
-      def pickup_day_params
-        params.require(:pickup_day).permit(:scheduled_for, :organization_id, :starts_at, :ends_at)
-      end
+  def delete
+
+  end
+
+  private
+
+  def pickup_day_params
+    params.require(:pickup_day).permit(:scheduled_for, :organization_id, :starts_at, :ends_at)
+  end
 end
